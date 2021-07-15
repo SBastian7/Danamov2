5b9fa4ce6ad569b50b25174f20fd5c9459381acb
SBastian7 <semolina@utp.edu.co>
2021-07-14 21:43:30 -0500

    Added Redux to HomeScreen

---
+++ frontend/src/data.js
@@ -0,0 +1,99 @@
+const data = {
+    products:[
+        {
+            _id:'0',
+            name:'Ultra Highness',
+            category:'Camisetas',
+            image:'item.jpg',
+            price:143900,
+            brand:'Jordan',
+            rating:4.5,
+            stock:0,
+            numReviews:10,
+            description:'Ultra ligera camisetas sisaso nonas dm ioasjpo wskjs wqop',
+            colores:[
+                'red',
+                'green',
+                'blue'
+            ],
+            tallas:[
+                15,
+                16,
+                17
+            ]
+        },
+        {
+            _id:'1',
+            name:'Camiseta Hoiwjd',
+            category:'Camisetas',
+            image:'p0.jpg',
+            price:185000,
+            brand:'Nike',
+            rating:2.8,
+            stock:15,
+            numReviews:10,
+            description:'Ultra ligera camisetas sisaso nonas dm ioasjpo wskjs wqop',
+            colores:[
+                'red',
+                'green',
+                'yellow'
+            ],
+            tallas:[
+                15,
+                17
+            ]
+        },
+        {
+            _id:'2',
+            name:'Caremañola MFXd',
+            category:'Camisetas',
+            image:'item.jpg',
+            price:185000,
+            brand:'Nike',
+            rating:2.,
+            stock:18,
+            numReviews:10,
+            description:'Ultra ligera camisetas sisaso nonas dm ioasjpo wskjs wqop',
+            colores:[
+                'red'
+            ],
+            tallas:[
+                15
+            ]
+        },
+        {
+            _id:'3',
+            name:'Buso Housel Hans',
+            category:'Zapatos',
+            image:'item.jpg',
+            price:125000,
+            brand:'Puma',
+            rating:4.8,
+            stock:18,
+            numReviews:3,
+            description:'Ultimos nuevos y mazimos dm ioasjpo wskjs wqop',
+            colores:[],
+            tallas:[]
+        },
+        {
+            _id:'4',
+            name:'Camiseta Hoiwjd',
+            category:'Camisetas',
+            image:'item.jpg',
+            price:85000,
+            brand:'Adidas',
+            rating:3.5,
+            stock:18,
+            numReviews:10,
+            description:'una mas por qwuyhqwuier asopkdm ioasjpo wskjs wqop',
+            colores:[
+                'red'
+            ],
+            tallas:[
+                15
+            ]
+        }
+    ]
+}
+
+export default data;
