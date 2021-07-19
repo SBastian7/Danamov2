691393939c23a12458bc0130e91683d991fb5be9
SBastian7 <semolina@utp.edu.co>
2021-07-19 13:23:16 -0500

    Create signin backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import dotenv from 'dotenv';
+
 
 const app = express();
-//process.env.MONGO_DB_URL
-mongoose.connect(`mongodb+srv://admin:admin@cluster0.ceftn.mongodb.net/danamo_db?retryWrites=true&w=majority`,{
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -19,7 +25,7 @@ app.use('/api/products', productRouter)
 app.use((err,req,res,next) => {
     res.status(500).send({ message: err.message })
 })
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4477
 app.listen(port, () => {
     console.log('Serving at '+port)
 })
